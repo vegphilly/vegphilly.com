0d73abb2a093fd5787367da6b63c8f76a7bf5672
Kevin DeLoach <kdeloach@azavea.com>
2014-10-11 17:33:32 -0400

    Add strict mode to each module
    These were already jshint compliant due to recent refactoring.

--- vegancity/js/src/map.js
+++ vegancity/js/src/map.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var _ = require('lodash'),
     $ = require('jquery'),
     google = require('google');
--- vegancity/js/src/profile_page.js
+++ vegancity/js/src/profile_page.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var $ = require('jquery');
 
 function init() {
